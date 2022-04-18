5d7ecefc64ee0adfc2f04ccc4d96652f9acaab57
Noskov Sergey <noskov.sergey@mail.ru>
2022-04-18 20:45:08 +0500

    причесал

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,9 +1,9 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.dispatch import receiver
-from django.db.models.signals import post_save
 from django.contrib.auth.tokens import default_token_generator
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class User(AbstractUser):
